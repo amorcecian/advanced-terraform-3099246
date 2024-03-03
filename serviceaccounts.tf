54b993c5e63603a714e0b141b97a376db5a260d9
Aram Morcecian <amorcecian@allurion.com>
2024-03-03 19:52:23 -0300

    asd

--- serviceaccounts.tf
+++ serviceaccounts.tf
@@ -3,9 +3,11 @@ module "service_accounts" {
   source        = "terraform-google-modules/service-accounts/google"
   project_id    = var.project-id
   prefix        = "viewer-sa"
-  names         = [for s in var.environment_list: lower(s)]
+  names         = ["dev", "qa","stage","prod"]
   project_roles = [
     "${var.project-id}=>roles/viewer",
     "${var.project-id}=>roles/storage.objectViewer",
   ]
+  grant_billing_role = true
+  org_id = var.org_id
 }
